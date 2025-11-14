84a2d8d1020acb2e3278d723f06262cf751c57d8
zohaibhatti <zohiabshahzadbhatti@gmail.com>
2025-11-15 03:46:54 +0500

    custom.js

--- autorepair/js/custom.js
+++ autorepair/js/custom.js
@@ -7,3 +7,21 @@
 //   }
 // });
 ;
+
+// <!========================================
+            // START BACK TO TOP 
+// ======================================== >
+let backToTopButton = document.getElementById("backToTop");
+window.onscroll = function() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        backToTopButton.style.display = "flex";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+backToTopButton.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+// <!========================================
+            // END BACK TO TOP
+// ======================================== >
