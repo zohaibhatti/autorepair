1ff52b855f447900ad649394b7a457489d789923
zohaibhatti <zohiabshahzadbhatti@gmail.com>
2025-11-15 02:50:46 +0500

    Add files via upload

---
+++ autorepair/js/custom.js
@@ -0,0 +1,9 @@
+// window.addEventListener("scroll", function() {
+//   const navbar = document.querySelector(".transparent-navbar");
+//   if (window.scrollY > 50) {
+//     navbar.classList.add("scrolled");
+//   } else {
+//     navbar.classList.remove("scrolled");
+//   }
+// });
+;
